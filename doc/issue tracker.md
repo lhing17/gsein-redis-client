a03b7e589062d73360cf88c3644816e3667d49f7
lianghao <lhing17@163.com>
2021-02-22 17:08:55 +0800

    feat：引入electron。

---
+++ doc/issue tracker.md
@@ -0,0 +1,9 @@
+1. electron 安装失败
+
+- Mac系统
+
+ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/ npm install
+
+- Windows系统
+
+set "ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron" && npm install
---
+++ src/main/electron/redis-electron/vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+function resolve (dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: './',
+  devServer: {
+    // can be overwritten by process.env.HOST
+    host: '0.0.0.0',
+    port: 8008
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('src', resolve('src'))
+      .set('common', resolve('src/common'))
+      .set('components', resolve('src/components'));
+  }
+};
